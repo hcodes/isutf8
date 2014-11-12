ab5c3fb65fc5844b2e51f93853ff97e2509fee1c
Denis Seleznev <hcodes@yandex.ru>
2014-11-12 22:37:33 +0300

    1.0.1
    Refactoring

--- bin/cli.js
+++ bin/cli.js
@@ -2,31 +2,29 @@
 
 var fs = require('fs'),
     isUtf8 = require('../lib/isutf8'),
-    program = require('commander');
+    program = require('commander'),
+    exitCode = 0;
 
 program
     .usage('<file ...>')
     .parse(process.argv);
 
-var resource = process.argv[2];
-
-if(!resource) {
+if(!process.argv[2]) {
     program.help();
 }
 
-var exitCode = 0;
 for(var i = 2; i < process.argv.length; i++) {
     var file = process.argv[i];
-    
+
     if(fs.existsSync(file)) {
         if(isUtf8(fs.readFileSync(file))) {
-            console.log(file + ' is UTF-8.');
+            console.log(file + ' is UTF-8');
         } else {
-            console.log(file + ' is NOT UTF-8.');
+            console.log(file + ' is NOT UTF-8');
             exitCode = 1;
         }
     } else {
-        console.log(resource + ': No such file');
+        console.log(file + ': No such file');
         exitCode = 1;
     }
 }
