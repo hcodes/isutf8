31fab5e7f01b786e267f74d841dc44b9b6beb02e
Denis Seleznev <hcodes@yandex.ru>
2014-11-12 23:57:40 +0300

    1.0.3

--- test/test.texts.js
+++ test/test.texts.js
@@ -1,25 +1,24 @@
 var fs = require('fs'),
     isUtf8 = require('../lib/isutf8');
-    console.log(isUtf8);
     var assert = require('chai').assert,
     getText = function(id) {
         return fs.readFileSync('./test/texts/' + id + '.txt');
     }
 
 describe('Examples', function() {
-    it('windows1251', function() {
+    it('file windows1251', function() {
         assert.notOk(isUtf8(getText('win1251')));
     });
 
-    it('koi8r', function() {
+    it('file koi8r', function() {
         assert.notOk(isUtf8(getText('koi8r')));
     });
 
-    it('utf8', function() {
+    it('file utf8', function() {
         assert.ok(isUtf8(getText('utf8')));
     });
 
-    it('ansi', function() {
+    it('file ansi', function() {
         assert.ok(isUtf8(getText('ansi')));
     });
 });
